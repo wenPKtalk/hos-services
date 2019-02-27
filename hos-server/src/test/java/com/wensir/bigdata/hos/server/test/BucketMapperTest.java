8e62cec3bd094d8aa0c3c918116b3140b55fa890
wensir <wenpengkun@kyee.com.cn>
2019-02-27 22:31:36 +0800

    测试类的开发

---
+++ hos-server/src/test/java/com/wensir/bigdata/hos/server/test/BucketMapperTest.java
@@ -0,0 +1,64 @@
+package com.wensir.bigdata.hos.server.test;
+
+
+import com.wensir.bigdata.hos.core.auth.model.ServiceAuth;
+import com.wensir.bigdata.hos.core.auth.service.IAuthService;
+import com.wensir.bigdata.hos.core.user.model.SystemRole;
+import com.wensir.bigdata.hos.core.user.model.UserInfo;
+import com.wensir.bigdata.hos.core.user.service.IUserInfoService;
+import com.wensir.bigdata.hos.server.BucketModel;
+import com.wensir.bigdata.hos.server.dao.BucketModelMapper;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.wensir.bigdata.hos.mybatis.test.*;
+
+import java.util.List;
+
+
+public class BucketMapperTest{
+
+  @Autowired
+  BucketModelMapper bucketMapper;
+  @Autowired
+  IAuthService authService;
+  @Autowired
+  IUserInfoService userService;
+
+  @Test
+  public void addBucket() {
+    BucketModel bucketModel = new BucketModel("test1", "jixin", "");
+    bucketMapper.addBucket(bucketModel);
+    UserInfo userInfo = new UserInfo("jixin", "123456","", SystemRole.ADMIN);
+    userService.addUser(userInfo);
+    ServiceAuth serviceAuth = new ServiceAuth();
+    serviceAuth.setTargetToken(userInfo.getUserId());
+    serviceAuth.setBucketName(bucketModel.getBucketName());
+    authService.addAuth(serviceAuth);
+    BucketModel bucketModel2 = new BucketModel("test2", "jixin", "");
+    bucketMapper.addBucket(bucketModel2);
+  }
+
+  @Test
+  public void getBucket() {
+    BucketModel bucketModel = bucketMapper.getBucketByName("test1");
+    System.out.println(bucketModel.getBucketId() + "|" + bucketModel.getBucketName());
+  }
+
+  @Test
+  public void getUserAuthorizedBuckets() {
+    UserInfo userInfo = userService.getUserInfoByName("jixin");
+    List<BucketModel> bucketModels = bucketMapper.getUserAuthorizedBuckets(userInfo.getUserId());
+    bucketModels.forEach(bucketModel -> {
+      System.out.println(bucketModel.getBucketId() + "|" + bucketModel.getBucketName());
+    });
+  }
+
+  @Test
+  public void deleteBucket() {
+    UserInfo userInfo = userService.getUserInfoByName("jixin");
+    List<BucketModel> bucketModels = bucketMapper.getUserAuthorizedBuckets(userInfo.getUserId());
+    bucketModels.forEach(bucketModel -> {
+      bucketMapper.deleteBucket(bucketModel.getBucketId());
+    });
+  }
+}
